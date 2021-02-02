ba166cb7e537bd30f87ba384cc9d32c19f95c02b
rchalamala <rchalamala@outlook.com>
2021-02-02 00:07:16 -0700

    Update

--- .caide/templates/solution_template.cpp
+++ .caide/templates/solution_template.cpp
@@ -1,29 +1,20 @@
-#include "general/base.h"
-#include "general/debug.h"
-#include "general/input.h"
-#include "general/output.h"
+#include <algorithm>
+#include <iomanip>
+#include <istream>
+#include <map>
+#include <numeric>
+#include <ostream>
+#include <set>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
 using namespace std;
-
-//#define INTERACTIVE
-//#define CODE_JAM
-//#define LIGHT_OJ
+// Powered by caide (code generator, tester, and library code inliner)
 
 class Solution {
-  void output_case(std::ostream& out) {
-    static std::int32_t _test_case = 0;
-    out << "Case #";
-#ifdef CODE_JAM
-    out << "#";
-#endif
-    out << ++_test_case << ": ";
-  }
-
  public:
-  void solve(std::istream& in, std::ostream& out) {
-#if defined(CODE_JAM) || defined(LIGHT_OJ)
-    output_case(out);
-#endif
-  }
+  void solve(std::istream& in, std::ostream& out) {}
 };
 
 void solve(std::istream& in, std::ostream& out) {
